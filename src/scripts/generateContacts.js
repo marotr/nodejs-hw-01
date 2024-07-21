63b3ca6714e535f39d620cc3904be34ecaab3a4e
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-07-21 15:57:14 +0300

    all tasks completed

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -3,28 +3,29 @@ import { PATH_DB } from '../constants/contacts.js';
 import { createFakeContact } from '../utils/createFakeContact.js';
 
 
-async function generateContacts(count) {
+const generateContacts = async (number) => {
   try {
 
-    const data = await fs.readFile(PATH_DB, { encoding: 'utf8' });
-    const contacts = JSON.parse(data);
+      const data = await fs.readFile(PATH_DB, { encoding: 'utf8' });
+      const contacts = JSON.parse(data);
 
 
-    const newContacts = [];
-    for (let i = 0; i < count; i++) {
-      newContacts.push(createFakeContact());
-    }
+      const newContacts = [];
+      for (let i = 0; i < number; i++) {
+          newContacts.push(createFakeContact());
+      }
 
 
-    const updatedContacts = [...contacts, ...newContacts];
+      const updatedContacts = [...contacts, ...newContacts];
 
 
-    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2), { encoding: 'utf8' });
+      await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2), { encoding: 'utf8' });
 
-    console.log(`Successfully added ${count} contacts to the database.`);
+      console.log(`Successfully added ${number} contacts to the database.`);
   } catch (error) {
-    console.error('Error:', error);
+      console.error('Error:', error);
   }
-}
-const count = process.argv[2] ? parseInt(process.argv[2], 10) : 5;
-generateContacts(count);
+};
+
+
+generateContacts(3);
